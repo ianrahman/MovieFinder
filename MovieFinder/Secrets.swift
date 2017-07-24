7f2fba5295a98c2bab2906ebbe2b1ec9616664ac
Ian Rahman <ian@evergreenlabs.io>
2017-07-24 17:18:37 -0400

    allow secrets file

---
+++ MovieFinder/Secrets.swift
@@ -0,0 +1,18 @@
+//
+//  Secrets.swift
+//  MovieFinder
+//
+//  Created by Ian Rahman on 7/20/17.
+//  Copyright © 2017 Evergreen Labs. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Secrets
+
+struct Secrets {
+    
+    static let apiKey = ""
+    
+}
+
